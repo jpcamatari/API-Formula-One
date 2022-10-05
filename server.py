34b3324abe98fc4dc5253ba3bc5124a1ffda4960
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-10-04 22:46:53 -0300

    ambiente virtual

--- server.py
+++ server.py
@@ -1,29 +1,21 @@
-from fastapi import FastAPI
-import uvicorn
 from model import CON
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI
 
-USUARIO = "root"
-SENHA = ""
-HOST = "localhost"
-BANCO = "formula_one"
-PORT = "3306"
 
-CON = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
-
-def conectaBanco():
-    engine = create_engine(CON, echo=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
+# def conectaBanco():
+#     engine = create_engine(CON, echo=True)
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+#     return session
 
   
 app = FastAPI()
 
 @app.get("/")
-def mostrarCircuitos():
+def root():
     #return session.query(Circuits).all()
     return {'ola':'mundo'}
     

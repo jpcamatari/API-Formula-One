a0fb7a91d6c9ec55e2559e4c38bc8d7246228c89
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-10-06 01:36:07 -0300

    Finalizado EndPoints e Models

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from sqlite3 import Date
 from typing import Optional
 from xmlrpc.client import DateTime
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
@@ -50,19 +51,119 @@ class Constructors(Base):
 class ConstructorResults(Base):
     __tablename__ = "constructor_results"
     constructorResultsId = Column(Integer, primary_key=True)
-    raceId = Column(Integer(10))
-    constructorId = Column(Integer(10))
-    points = Column(Integer(10))
+    raceId = Column(Integer)
+    constructorId = Column(Integer)
+    points = Column(Integer)
 
 class ConstructorStandings(Base):
     __tablename__ = "constructor_standings"
     constructorStandingsId = Column(Integer, primary_key=True)
     raceId = Column(String(10))
-    constructorId = Column(Integer(10))
-    points = Column(Integer(10))
-    position = Column(Integer(10))
+    constructorId = Column(Integer)
+    points = Column(Integer)
+    position = Column(Integer)
     positionText = Column(String(10))
-    wins = Column(Integer(10))
+    wins = Column(Integer)
+
+class DriverStandings(Base):
+    __tablename__ = "driver_standings"
+    driverStandingsId = Column(Integer, primary_key=True)
+    raceId = Column(String(10))
+    driverId = Column(Integer)
+    points = Column(Integer)
+    position = Column(Integer)
+    positionText = Column(String(10))
+    wins = Column(Integer)
+
+class Races(Base):
+    __tablename__ = "races"
+    raceId = Column(Integer, primary_key=True)
+    year = Column(String(10))
+    round = Column(Integer)
+    circuitId = Column(Integer)
+    name = Column(String(50))
+    data = Column(DateTime)
+    wins = Column(Integer)
+
+class PitStops(Base):
+    __tablename__ = "pit_stops"
+    raceId = Column(Integer, primary_key=True)
+    driverId = Column(Integer)
+    stop = Column(Integer)
+    lap = Column(Integer)
+    time = Column(String(50))
+    duration = Column(String(50))
+    milliseconds = Column(Integer)
+  
+class Qualifying(Base):
+    __tablename__ = "qualifying"
+    qualifyId = Column(Integer, primary_key=True)
+    raceId = Column(Integer)
+    driverId = Column(Integer)
+    constructorId = Column(Integer)
+    number = Column(Integer)
+    position = Column(Integer)
+    q1 = Column(String(50))
+    q2 = Column(String(50))
+    q3 = Column(String(50))
+
+class LapTimes(Base):
+    __tablename__ = "lap_times"
+    raceId = Column(Integer, primary_key=True)
+    driverId = Column(Integer)
+    lap = Column(Integer)
+    position = Column(Integer)
+    time = Column(String(50))
+    milliseconds = Column(Integer)
+
+class Results(Base):
+    __tablename__ = "results"
+    resultId = Column(Integer, primary_key=True)
+    raceId = Column(Integer)
+    driverId = Column(Integer)
+    constructorId = Column(Integer)
+    number = Column(Integer)
+    grid = Column(Integer)
+    position = Column(Integer)
+    positionText = Column(String(50))
+    positionOrder = Column(String(50))
+    points = Column(Integer)
+    laps = Column(Integer)
+    time = Column(String(50))
+    milliseconds = Column(Integer)
+    fastestLap = Column(Integer)
+    rank = Column(Integer)
+    fastestLapTime = Column(String(50))
+    fastestLapSpeed = Column(String(50))
+    statusId = Column(Integer)
+
+class SprintResults(Base):
+    __tablename__ = "sprint_results"
+    resultId = Column(Integer, primary_key=True)
+    raceId = Column(Integer)
+    driverId = Column(Integer)
+    constructorId = Column(Integer)
+    number = Column(Integer)
+    grid = Column(Integer)
+    position = Column(Integer)
+    positionText = Column(String(50))
+    positionOrder = Column(String(50))
+    points = Column(Integer)
+    laps = Column(Integer)
+    time = Column(String(50))
+    milliseconds = Column(Integer)
+    fastestLap = Column(Integer)
+    rank = Column(Integer)
+    fastestLapTime = Column(String(50))
+    fastestLapSpeed = Column(String(50))
+    statusId = Column(Integer)
+
+class Status(Base):
+    __tablename__ = "status"
+    statusId = Column(Integer, primary_key=True)
+    status = Column(String(50))
+    
+
 
 Base.metadata.create_all(engine)
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from model import CON, ConstructorResults, ConstructorStandings, Constructors, session, Circuits, Drivers, Constructor
+from model import session, LapTimes, PitStops, Qualifying, Results, SprintResults, Status, ConstructorResults, ConstructorStandings, Constructors, DriverStandings, Races, Circuits, Drivers
 from fastapi import FastAPI
  
 app = FastAPI()
@@ -22,5 +22,41 @@ def allConstructor_results():
 @app.get("/constructor-standings")
 def allConstructor_standings():
     return session.query(ConstructorStandings).all()
+
+@app.get("/driver-standings")
+def allDriver_standings():
+    return session.query(DriverStandings).all()
+
+@app.get("/races")
+def allRaces():
+    return session.query(Races).all()
+
+@app.get("/pit-stops")
+def allPit_stops():
+    return session.query(PitStops).all()
+
+@app.get("/qualifying")
+def allQualifying():
+    return session.query(Qualifying).all()
+
+@app.get("/lap-times")
+def allLaps():
+    return session.query(LapTimes).all()
+
+@app.get("/results")
+def allResults():
+    return session.query(Results).all()
+
+@app.get("/sprint-results")
+def allSprintResults():
+    return session.query(SprintResults).all()
+
+@app.get("/status")
+def allStatus():
+    return session.query(Status).all()
+
+
+
+
 
 
